736d034ddfaedd941f678d9dfce823b1e6dc9ca7
Robert Flaherty <roberte.flaherty@gmail.com>
2020-10-26 09:33:34 -0400

    half of senator data has been input

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Senator} from '../hero';
+import {Senator} from '../senator';
 
 @Component({
   selector: 'app-heroes',
--- src/app/senator.ts
+++ src/app/senator.ts
@@ -1,4 +1,4 @@
-export interface Hero {
+export interface Senator {
   state: string;
   name: string;
   party: string;
