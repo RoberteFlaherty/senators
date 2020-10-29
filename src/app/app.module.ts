da80e067d759330a43aeacbd423772522a60d09c
RoberteFlaherty <45772389+RoberteFlaherty@users.noreply.github.com>
2020-10-29 15:17:15 -0400

    working on horizontal display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { SenatorsComponent } from './senators/senators.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/senators/senators.component.ts
+++ src/app/senators/senators.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Senator } from '../senator';
 import { SENATORS } from '../mock-senators';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @Component({
